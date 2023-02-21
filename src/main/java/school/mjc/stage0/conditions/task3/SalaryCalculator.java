0810fd74d229f3fe073631a9a04a1edd7d33fea4
Bakhovuddin Latifjanov <salokhiddin2016@Bakhovuddins-MacBook-Pro.local>
2023-02-21 17:17:30 +0500

    Multiple conditions

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,20 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        double salaryAfterTax;
+        if (salary > 0) {
+            if (salary <= 10000) {
+                salaryAfterTax =salary-(salary * 0.15);
+                System.out.println(salaryAfterTax);
+            } else if (salary <= 20000) {
+                salaryAfterTax =salary-(salary * 0.18);
+                System.out.println(salaryAfterTax);
+            } else {
+                salaryAfterTax=salary-(salary/5);
+                System.out.println(salaryAfterTax);
+            }
+        } else {
+            System.out.println("wrong input!");
+        }
     }
 }
